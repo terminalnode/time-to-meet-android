a8a27957576d484d336fabcc859246454b0517d0
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-05-18 11:04:49 +0200

    move activities to separate package

--- app/src/main/java/com/example/timetomeet/activity/SplashActivity.java
+++ app/src/main/java/com/example/timetomeet/activity/SplashActivity.java
@@ -1,14 +1,16 @@
-package com.example.timetomeet;
+package com.example.timetomeet.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.timetomeet.R;
+
+public class SplashActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_splash);
   }
 }
