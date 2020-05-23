8390f7e97b0c1bdcd3496f1e778c052133826a04
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-05-23 23:54:48 +0200

    make seating entity parcelable

--- app/src/main/java/com/example/timetomeet/retrofit/entity/Seating.java
+++ app/src/main/java/com/example/timetomeet/retrofit/entity/Seating.java
@@ -1,8 +1,11 @@
 package com.example.timetomeet.retrofit.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class Seating {
+public class Seating implements Parcelable {
   @SerializedName("id")
   private Long id;
 
@@ -29,6 +32,38 @@ public class Seating {
         ", iconPath='" + iconPath + '\'' +
         '}';
   }
+
+  @Override
+  public int describeContents() {
+    return 0;
+  }
+
+  @Override
+  public void writeToParcel(Parcel dest, int flags) {
+    dest.writeValue(this.id);
+    dest.writeString(this.nameSv);
+    dest.writeString(this.nameEn);
+    dest.writeString(this.iconPath);
+  }
+
+  protected Seating(Parcel in) {
+    this.id = (Long) in.readValue(Long.class.getClassLoader());
+    this.nameSv = in.readString();
+    this.nameEn = in.readString();
+    this.iconPath = in.readString();
+  }
+
+  public static final Parcelable.Creator<Seating> CREATOR = new Parcelable.Creator<Seating>() {
+    @Override
+    public Seating createFromParcel(Parcel source) {
+      return new Seating(source);
+    }
+
+    @Override
+    public Seating[] newArray(int size) {
+      return new Seating[size];
+    }
+  };
 
   //----- Setters -----//
   public void setId(Long id) {
