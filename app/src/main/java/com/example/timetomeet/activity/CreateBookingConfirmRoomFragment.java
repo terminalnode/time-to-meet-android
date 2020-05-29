931930b015883be87efc24e792b31c522fb68552
Alexander <2683344+terminalnode@users.noreply.github.com>
2020-05-29 15:11:25 +0200

    do find views in ConfirmRoomFragment#onViewCreated

--- app/src/main/java/com/example/timetomeet/activity/CreateBookingConfirmRoomFragment.java
+++ app/src/main/java/com/example/timetomeet/activity/CreateBookingConfirmRoomFragment.java
@@ -1,9 +1,7 @@
 package com.example.timetomeet.activity;
 
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
 import android.text.InputFilter;
-import android.text.Spanned;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +10,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -28,13 +25,18 @@ import com.example.timetomeet.retrofit.entity.AvailableRoom;
 import com.example.timetomeet.retrofit.entity.ConferenceRoom;
 import com.example.timetomeet.retrofit.entity.ConferenceRoomSeating;
 import com.example.timetomeet.retrofit.entity.PaymentAlternative;
-import com.example.timetomeet.retrofit.entity.Seating;
 import com.google.android.material.snackbar.Snackbar;
-
-import java.util.List;
 
 public class CreateBookingConfirmRoomFragment extends Fragment {
   AvailableRoom selectedRoom;
+  EditText specialRequestMultiLineText;
+  EditText numberOfParticipantsEditText;
+  Spinner paymentAlternativeSpinner;
+  Spinner seatingChoiceSpinner;
+  RadioGroup timeSlotRadioGroup;
+  RadioGroup wantRoomInfoRadioGroup;
+  RadioGroup wantActivityInfoRadioGroup;
+  ProgressBar progressBar;
 
   @Override
   public View onCreateView(
@@ -52,8 +54,17 @@ public class CreateBookingConfirmRoomFragment extends Fragment {
     CreateBookingActivity activity = (CreateBookingActivity) getActivity();
     selectedRoom = bookingBundle.getParcelable(Helper.BUNDLE_SELECTED_ROOM);
 
+    // Find views
+    specialRequestMultiLineText = view.findViewById(R.id.specialRequestMultilineText);
+    numberOfParticipantsEditText = view.findViewById(R.id.numberOfParticipantsEditText);
+    paymentAlternativeSpinner = view.findViewById(R.id.paymentAlternativeSpinner);
+    seatingChoiceSpinner = view.findViewById(R.id.seatingChoiceSpinner);
+    timeSlotRadioGroup = view.findViewById(R.id.timeSlotRadioGroup);
+    wantRoomInfoRadioGroup = view.findViewById(R.id.wantRoomInfoRadioGroup);
+    wantActivityInfoRadioGroup = view.findViewById(R.id.wantActivityInfoRadioGroup);
+    progressBar = view.findViewById(R.id.progressBar);
+
     // Set up payment alternative spinner
-    Spinner paymentAlternativeSpinner = view.findViewById(R.id.paymentAlternativeSpinner);
     PaymentAlternativeSpinnerAdapter paymentAdapter = new PaymentAlternativeSpinnerAdapter(
         getContext(),
         R.layout.single_payment_alternative,
@@ -62,14 +73,17 @@ public class CreateBookingConfirmRoomFragment extends Fragment {
     paymentAlternativeSpinner.setAdapter(paymentAdapter);
 
     // Set up seating spinner
-    EditText numParticipantsEditText = view.findViewById(R.id.numberOfParticipantsEditText);
-    Spinner seatingChoiceSpinner = view.findViewById(R.id.seatingChoiceSpinner);
     SeatingSpinnerAdapter seatingAdapter = new SeatingSpinnerAdapter(
         getContext(),
         selectedRoom.getAssociatedConferenceRoom().getDefaultSeating(),
         activity.getSeatingMap(),
-        numParticipantsEditText
+        numberOfParticipantsEditText
     );
+    seatingChoiceSpinner.setAdapter(seatingAdapter);
+
+    // Make sure the user doesn't input anything stupid
+    numberOfParticipantsEditText.addTextChangedListener(
+        new ParticipantsNumberTextWatcher(seatingChoiceSpinner));
 
     // Set input filters to agreement number
     EditText agreementNumber = view.findViewById(R.id.agreementNumberEditText);
@@ -78,55 +92,45 @@ public class CreateBookingConfirmRoomFragment extends Fragment {
         new AlphaNumericFilter()
     });
 
-    seatingChoiceSpinner.setAdapter(seatingAdapter);
-
+    // Set on confirm room button-click
     view.findViewById(R.id.confirmRoomButton)
         .setOnClickListener(this::confirmRoomButtonClicked);
-
-    numParticipantsEditText.addTextChangedListener(
-        new ParticipantsNumberTextWatcher(seatingChoiceSpinner));
   }
 
 
 
-  private void confirmRoomButtonClicked(View v) {
-    View view = getView();
-
-    EditText specialRequestMultiLineText = view.findViewById(R.id.specialRequestMultilineText);
-    EditText numberOfParticipantsEditText = view.findViewById(R.id.numberOfParticipantsEditText);
+  private void confirmRoomButtonClicked(View view) {
     String specialRequest = specialRequestMultiLineText.getText().toString();
     int numberOfParticipants;
     try {
       numberOfParticipants = Integer.parseInt(numberOfParticipantsEditText.getText().toString());
     } catch (NumberFormatException e) {
       Log.e(Logging.CreateBookingActivity, "Could not parse number of participants as int");
-      Snackbar.make(view, R.string.invalid_number_of_participants, Snackbar.LENGTH_LONG).show();
+      Snackbar.make(
+          view,
+          R.string.invalid_number_of_participants,
+          Snackbar.LENGTH_LONG
+      ).show();
       return;
     }
 
     // Get seating and payment alternative
-    Spinner paymentAlternativeSpinner = view.findViewById(R.id.paymentAlternativeSpinner);
-    Spinner seatingChoiceSpinner = view.findViewById(R.id.seatingChoiceSpinner);
     PaymentAlternative payment = (PaymentAlternative) paymentAlternativeSpinner.getSelectedItem();
     ConferenceRoomSeating seating = (ConferenceRoomSeating) seatingChoiceSpinner.getSelectedItem();
 
     ConferenceRoom conferenceRoom = selectedRoom.getAssociatedConferenceRoom();
-    ProgressBar progressBar = view.findViewById(R.id.progressBar);
 
     // Check if user wants activity info
-    RadioGroup wantActivityInfoRadioGroup = view.findViewById(R.id.wantActivityInfoRadioGroup);
     boolean wantActivityInfo = wantActivityInfoRadioGroup
         .getCheckedRadioButtonId() == R.id.yesActivityInfoRadioButton;
 
     // Check if user wants room info
-    RadioGroup wantRoomInfoRadioGroup = view.findViewById(R.id.wantRoomInfoRadioGroup);
     boolean wantRoomInfo = wantRoomInfoRadioGroup
         .getCheckedRadioButtonId() == R.id.yesRoomInfoRadioButton;
 
     // Check which time slots the user wants to book.
     // id31 is the before noon time slot.
     // id32 is the afternoon time slot.
-    RadioGroup timeSlotRadioGroup = view.findViewById(R.id.timeSlotRadioGroup);
     long[] listOfTimeSlots;
     switch (timeSlotRadioGroup.getCheckedRadioButtonId()) {
       case R.id.amRadioButton:
